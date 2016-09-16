f09d916a19c98e458f2ae4ff0041ac7a88410f3a
gd123yjy <1224624112@qq.com>
2016-09-16 16:29:14 +0800

    resolve the invalidated delete bug when deleting journal because of the reference from paragraph to user

--- src/com/dao/impl/JournalDAOImpl.java
+++ src/com/dao/impl/JournalDAOImpl.java
@@ -1,8 +1,10 @@
 package com.dao.impl;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
+import com.bean.Journal;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -10,6 +12,7 @@ import org.hibernate.SessionFactory;
 import com.bean.Chapter;
 import com.bean.Paragraph;
 import com.dao.JournalDAO;
+import org.hibernate.Transaction;
 
 public class JournalDAOImpl implements JournalDAO {
 
@@ -132,9 +135,17 @@ public class JournalDAOImpl implements JournalDAO {
 	@Override
 	public void delete_journal(Integer journal_id) {
 		Session session = sessionFactory.openSession();
-		String hql = "delete from Journal journal where journal_id = " + journal_id;
-		Query query = session.createQuery(hql);
-		query.executeUpdate();
+		//删除相关的articles
+		//List list = find_article_of_journal(journal_id);
+
+		//删除journal
+		Transaction t = session.beginTransaction();
+//		String hql = "delete from Journal journal where journal_id = " + journal_id;
+//		Query query = session.createQuery(hql);
+//		query.executeUpdate();
+		Journal journal = (Journal) session.get(Journal.class,journal_id);
+		session.delete(journal);
+		t.commit();
 		session.close();
 	}
 
